9ee4f891e277164001f9ebee68ddee4a7a206637
James Lyne <jim+github@not-null.co.uk>
2025-01-11 21:42:46 +0000

    Refactor gradle scripts
    - Switch to Kotlin
    - Switch to plugin-yml gradle plugin
    - Switch to using version catalog for dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+    alias(libs.plugins.pluginYmlPaper)
+}
+
+group = "uk.co.notnull"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_21
+java.targetCompatibility = JavaVersion.VERSION_21
+
+repositories {
+	mavenLocal()
+	mavenCentral()
+	maven {
+		url = uri("https://repo.papermc.io/repository/maven-public/")
+	}
+}
+
+dependencies {
+	compileOnly(libs.paperApi)
+}
+
+paper {
+    main = "uk.co.notnull.messagecommands.MessageCommands"
+    apiVersion = libs.versions.paperApiVersion.get().replace(Regex("\\-R\\d.\\d-SNAPSHOT"), "")
+    authors = listOf("Jim (AnEnragedPigeon)")
+    description = "Simple plugin for configurable canned message commands"
+}
+
+tasks {
+    compileJava {
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+        options.encoding = "UTF-8"
+    }
+}
